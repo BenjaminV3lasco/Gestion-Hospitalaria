c65b9dcbf8d23de7a67717f5fd11ac131b937bfc
Benjamín Velasco <velascobenjamin14@gmail.com>
2022-11-13 17:25:08 -0300

    Version 2

---
+++ src/trabajo/pkgfinal/paradigmas/Persona.java
@@ -0,0 +1,10 @@
+
+package trabajo.pkgfinal.paradigmas;
+
+public class Persona {
+    public int edad;
+    public String nombre;
+    public boolean rol; //Verdadero Médico, Falso Paciente
+    public int altura;
+    
+}
--- src/trabajo/pkgfinal/paradigmas/TrabajoFinalParadigmas.java
+++ src/trabajo/pkgfinal/paradigmas/TrabajoFinalParadigmas.java
@@ -4,7 +4,7 @@ package trabajo.pkgfinal.paradigmas;
 public class TrabajoFinalParadigmas {
 
     public static void main(String[] args) {
-        
+       
     }
     
 }
